2c24201811995193a301c9e84a2c24dea2ea57af
Abhisht Chouhan <abhishtchouhan@gmail.com>
2024-04-29 15:14:34 +0530

    Routes

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
-from routes import copilot_bp_fun
+from routes.copilot import bp
 
 def project_routes(app):
-    app.register_blueprint(copilot_bp_fun.copilot_bp, url_prefix='/copilot')
+    app.register_blueprint(bp, url_prefix='/copilot')
 
     return app
