bf2d908cc4a1f7d038018eea5c5ac146bc5429e6
Abhisht Chouhan <abhishtchouhan@gmail.com>
2024-05-02 08:05:49 +0530

    Fixed vercel

--- api/app.py
+++ api/app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, send_file
 from flask_cors import CORS
 # from routes import project_routes
 import logging
-from functions.copilot_gen import generate_text
+from api.functions.copilot_gen import generate_text
 import os
 import random
 from io import BytesIO
